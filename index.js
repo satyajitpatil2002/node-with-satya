c10308cde8d7c6edcc86d4d06bac93c707f9e79b
satyajitpatil2002 <105158573+satyajitpatil2002@users.noreply.github.com>
2024-07-08 07:42:02 +0000

    c

--- index.js
+++ index.js
@@ -1,43 +1,93 @@
-const { log } = require("console");
-const http=require("http");
-const fs = require("fs");
-const url= url
-//const express=require("express");
+const http = require("http");
+const express = require("express");
 
-
-// const app= express // app is an handekar function 
-// app.get('/',(req,res)=>{
+const app = express(); // app is an instance of express
+// app.get("/", (req, res) => {
 //     return res.send("hello from homepage");
 // });
-// app.get('/about',(req,res)=>{
-//     return res.send("hello from about page");
-// });
-
-// function.myHandler(req,res){
-//     if 
-// }
-
-
-
-//const  myServer= http.createServer(app);
-
-const  myServer= http.createServer((req,res)=>{
-    const log=`${Date.now()}: ${req.url} new request reciveed \n`;
-    fs.appendFile('log.txt',log,(error,data)=> {
-
-        switch(req,url){
-            case '/':res.end("homepage");
-            break;
-            case '/about':res.end("i am satya ");
-            break;
-            default:
-                res.end("404 not found ");
-
-
-
-        }
-    });
-    res.end("hellow from  satya Server again ");
-
+app.get("/about", (req, res) => {
+    return res.send("hello from about page");
 });
-myServer.listen(8000,()=> console.log("server started"));
+
+const myServer = http.createServer(app);
+
+myServer.listen(3000, () => console.log("server started on port 3000"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { log } = require("console");
+// const http=require("http");
+// const fs = require("fs");
+// const url= url
+// //const express=require("express");
+
+
+// // const app= express // app is an handekar function 
+// // app.get('/',(req,res)=>{
+// //     return res.send("hello from homepage");
+// // });
+// // app.get('/about',(req,res)=>{
+// //     return res.send("hello from about page");
+// // });
+
+// // function.myHandler(req,res){
+// //     if 
+// // }
+
+
+
+// //const  myServer= http.createServer(app);
+
+// const  myServer= http.createServer((req,res)=>{
+//     const log=`${Date.now()}: ${req.url} new request reciveed \n`;
+//     fs.appendFile('log.txt',log,(error,data)=> {
+
+//         switch(req,url){
+//             case '/':res.end("homepage");
+//             break;
+//             case '/about':res.end("i am satya ");
+//             break;
+//             default:
+//                 res.end("404 not found ");
+
+
+
+//         }
+//     });
+//     res.end("hellow from  satya Server again ");
+
+// });
+// myServer.listen(8000,()=> console.log("server started"));
